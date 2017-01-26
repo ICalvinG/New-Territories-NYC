b6a9b884bc0e064d9b0477a7765218a8a6332c16
icalving <calvinieong1118@gmail.com>
2017-01-25 22:19:52 -0500

    Add carousel container under menu

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -2,5 +2,6 @@ class WebsiteController < ApplicationController
   def index
   	@background_image = "/assets/ube.jpg"
   	@parallax_image = "/assets/unicorn.jpg"
+  	@thaigo_image = "/assets/affogato.jpg"
   end
 end
