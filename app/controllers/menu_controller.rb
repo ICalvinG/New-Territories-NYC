a60c602f87df8468bfdb7c883ea8486581c4f866
icalving <calvinieong1118@gmail.com>
2017-01-31 15:28:44 -0500

    Delete thaigo1 image and add carousel and images to menu view and controller

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,5 +1,9 @@
 class MenuController < ApplicationController
   def index
   	@background_image = "/assets/thaigo.jpg"
+  	@uni_image = "/assets/uni.jpg"
+  	@thaigo_image = "/assets/affogato.jpg"
+  	@cny_image = "/assets/cny.jpg"
+  	@unicorn_image = "/assets/unicorn.jpg"
   end
 end
