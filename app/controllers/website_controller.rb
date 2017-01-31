443e7f0205cc375cd420e47f39eb69305eebe605
icalving <calvinieong1118@gmail.com>
2017-01-31 15:23:14 -0500

    Add images and edit parallax image

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,7 +1,6 @@
 class WebsiteController < ApplicationController
   def index
   	@background_image = "/assets/ube.jpg"
-  	@parallax_image = "/assets/unicorn.jpg"
-  	@thaigo_image = "/assets/affogato.jpg"
+  	@parallax_image = "/assets/affogato.jpg"
   end
 end
