47a8d6b2e59f2ac7c9321535efc114f65ecdf40c
icalving <calvinieong1118@gmail.com>
2017-01-31 22:33:16 -0500

    Create form and send email on submit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,21 @@
 class JobsController < ApplicationController
+  
   def index
   	@background_image = "/assets/store.jpg"
   end
 
   def create
-  	binding.pry
+  	param = params["job"]
+  	@job = Job.new(first_name: param["first_name"], last_name: param["last_name"], email: param["email"])
+   	@job.save
+   		if @job.save
+   			job = Job.last
+   			mail = JobMailer.job_application(job)
+   			mail.deliver_now
+    	else
+      		@errors = "Something is wrong, please try again."
+      		render 'index'
+    	end
   end
+  
 end
