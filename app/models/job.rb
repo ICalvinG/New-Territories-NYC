1a3d9936ed299313ea9fd2a3a07acf54d1bd8b59
icalving <calvinieong1118@gmail.com>
2017-02-12 12:27:11 -0500

    Mount uploader in Job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
+	mount_uploader :resume, ResumeUploader
 
 	validates :first_name, :last_name, presence: true
 	validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
