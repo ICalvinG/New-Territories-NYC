ed83ba404663a64969e1643ed9ea82a9368e2214
icalving <calvinieong1118@gmail.com>
2017-02-12 20:21:23 -0500

    Create strong params for Job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,8 +10,7 @@ class JobsController < ApplicationController
   end
 
   def create
-  	param = params["job"]
-  	@job = Job.new(first_name: param["first_name"], last_name: param["last_name"], email: param["email"], resume: param["resume"], messages: param["messages"])
+  	@job = Job.new(job_params)
    	@job.save
    		if @job.save
    			job = Job.last
@@ -25,6 +24,12 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
     @job.destroy
     redirect_to jobs_path, notice:  "The job #{@job.name} has been deleted."
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:first_name, :last_name, :email, :resume, :messages)
   end
 
 end
